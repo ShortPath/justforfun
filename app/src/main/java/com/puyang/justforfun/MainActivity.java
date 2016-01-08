e39ae2a190061debeff120ec4c3a9d0bf0f215dc
杨浦 <yangpu0315@gmail.com>
2016-01-08 09:43:38 +0800

    initial commit

---
+++ app/src/main/java/com/puyang/justforfun/MainActivity.java
@@ -0,0 +1,100 @@
+package com.puyang.justforfun;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+interface Ladao{
+    default String displayFeature(){
+        return "ladaoba";
+    }
+}
+
+class LadaoClass implements Ladao{
+    @Override
+    public String displayFeature() {
+        return null;
+    }
+}
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        for (String s : Arrays.asList("a", "b", "c")) {
+            
+        }
+        Log.i("info", "onCreate");
+        new AlertDialog.Builder(this).setSingleChoiceItems(new String[]{"1", "2", "3"}, 0, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        }).show();
+
+        new AlertDialog.Builder(this).setMultiChoiceItems(new String[]{"1", "2", "3"}, new boolean[]{false, false, true}, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+
+            }
+        }).show();
+
+        ListView listView = new ListView(this);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        if(newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            setContentView(R.layout.activity_main);
+            Log.i("info", "portrait"); // 竖屏
+        }
+
+        if(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE)
+            setContentView(R.layout.activity_main);
+            Log.i("info", "landscape"); // 横屏
+    }
+}
---
+++ app/src/main/java/view/pager/indicator/PageIndicator.java
@@ -0,0 +1,12 @@
+package view.pager.indicator;
+
+import android.support.v4.view.ViewPager;
+
+/**
+ * Created by yangpu on 9/4/15.
+ */
+public interface PageIndicator extends ViewPager.OnPageChangeListener {
+
+
+
+}
