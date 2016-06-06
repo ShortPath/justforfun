e0e258f8dffef30fcd4c8cb998e2b82bbcd6dd55
杨浦 <yangpu0315@gmail.com>
2016-06-06 19:11:13 +0800

    try colorful plan

--- app/src/main/java/com/puyang/justforfun/MainActivity.java
+++ app/src/main/java/com/puyang/justforfun/MainActivity.java
@@ -1,40 +1,37 @@
 package com.puyang.justforfun;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.puyang.R;
+import com.puyang.animation.AllAnimation;
+import com.puyang.colorful.Colorful;
+import com.puyang.colorful.ViewGroupSetter;
 
-interface Ladao{
-    public String displayFeature();
-}
+public class MainActivity extends Activity{
 
-class LadaoClass implements Ladao{
-    @Override
-    public String displayFeature() {
-        return null;
-    }
-}
-
-public class MainActivity extends Activity {
-
-    @Override
+    private int symbol = 0;
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setTheme(R.style.LightAppTheme);
         setContentView(R.layout.activity_main);
         Log.i("info", "onCreate");
-        /*new AlertDialog.Builder(this).setSingleChoiceItems(new String[]{"1", "2", "3"}, 0, new DialogInterface.OnClickListener() {
+        *//*new AlertDialog.Builder(this).setSingleChoiceItems(new String[]{"1", "2", "3"}, 0, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
 
@@ -54,9 +51,133 @@ public class MainActivity extends Activity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
             }
-        });*/
+        });*//*
+        findViewById(R.id.favorite_list_edit_button).setOnClickListener(this);
+    }*/
+
+    ListView mNewsListView;
+    List<String> mNewsList = new ArrayList<String>();
+    Colorful mColorful;
+    boolean isNight = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_colorful);
+
+        // 换肤事件
+        findViewById(R.id.change_btn).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                changeThemeWithColorful();
+            }
+        });
+
+        // 第二个Activity,显示的是RecyclerVoew
+        findViewById(R.id.second_btn).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,
+                        AllAnimation.class);
+                startActivity(intent);
+            }
+        });
+
+        mNewsListView = (ListView) findViewById(R.id.listview);
+        // 模拟数据
+        mockNews();
+        mNewsListView.setAdapter(new NewsAdapter());
+
+        // 初始化Colorful
+        setupColorful();
     }
 
+    /**
+     * 设置各个视图与颜色属性的关联
+     */
+    private void setupColorful() {
+        ViewGroupSetter listViewSetter = new ViewGroupSetter(mNewsListView);
+        // 绑定ListView的Item View中的news_title视图，在换肤时修改它的text_color属性
+        listViewSetter.childViewTextColor(R.id.news_title, R.attr.text_color);
+
+        // 构建Colorful对象来绑定View与属性的对象关系
+        mColorful = new Colorful.Builder(this)
+                .backgroundDrawable(R.id.root_view, R.attr.root_view_bg)
+                        // 设置view的背景图片
+                .backgroundColor(R.id.change_btn, R.attr.btn_bg)
+                        // 设置背景色
+                .textColor(R.id.textview, R.attr.text_color)
+                .setter(listViewSetter) // 手动设置setter
+                .create(); // 设置文本颜色
+    }
+
+    /**
+     * 切换主题
+     */
+    private void changeThemeWithColorful() {
+        if (!isNight) {
+            mColorful.setTheme(R.style.DarkAppTheme);
+        } else {
+            mColorful.setTheme(R.style.LightAppTheme);
+        }
+        isNight = !isNight;
+    }
+
+    private void mockNews() {
+        for (int i = 0; i < 20; i++) {
+            mNewsList.add("News Title - " + i);
+        }
+    }
+
+    /**
+     *
+     * @author mrsimple
+     *
+     */
+    class NewsAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return mNewsList.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return mNewsList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            NewsViewHolder viewHolder = null;
+            if (convertView == null) {
+                convertView = LayoutInflater.from(parent.getContext()).inflate(
+                        R.layout.news_lv_item, parent, false);
+                viewHolder = new NewsViewHolder();
+                viewHolder.newsTitleView = (TextView) convertView
+                        .findViewById(R.id.news_title);
+
+                convertView.setTag(viewHolder);
+            } else {
+                viewHolder = (NewsViewHolder) convertView.getTag();
+            }
+
+            viewHolder.newsTitleView.setText(getItem(position));
+            return convertView;
+        }
+
+    }
+
+    public static class NewsViewHolder {
+        public TextView newsTitleView;
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -80,17 +201,4 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-
-        if(newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
-            setContentView(R.layout.activity_main);
-            Log.i("info", "portrait"); // 竖屏
-        }
-
-        if(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE)
-            setContentView(R.layout.activity_main);
-            Log.i("info", "landscape"); // 横屏
-    }
 }
